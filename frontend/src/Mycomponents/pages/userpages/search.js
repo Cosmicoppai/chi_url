5d1d4ef36ee511771915ae0ec5cff65a8c275201
Sushant shetty <72088338+tnaahsus@users.noreply.github.com>
2021-09-07 17:06:25 +0530

    Update search.js

--- frontend/src/Mycomponents/pages/userpages/search.js
+++ frontend/src/Mycomponents/pages/userpages/search.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
-
+//eslint-disable-next-line
 const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 export const Search = () => {

b0075eff3cdc46c3d7ea0810747f96f2dea2c6ff
Sushant shetty <72088338+tnaahsus@users.noreply.github.com>
2021-09-07 17:08:35 +0530

    Update home.js

--- frontend/src/Mycomponents/pages/Homepages/home.js
+++ frontend/src/Mycomponents/pages/Homepages/home.js
@@ -26,7 +26,7 @@ const Home = () => {
                 <main className="px-3 d-flex  justify-content-center flex-column" style={mystyle}>
                     <h1 className="text-center display-1" style={style}  >Welcome!</h1> 
                     <div className="col-lg-6 mx-auto">
-                        <p className="lead mb-4 text-center h3">We are URL shortener service that provide you to create short links from very long URLs. Our short links have the size of one third of the original URL, which makes them easier to type, present, or tweet. </p>
+                        <p className="lead mb-4 text-center h3">We are URL shortener service that provide you to create short links from very long URLs. Our short links have the size of seven characters only, which makes them easier to type, present, or tweet. </p>
                         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mt-2">
                             <Link to="/signup" type="button" className="btn btn-dark btn-lg px-4 gap-3">Sign up</Link>
                             <Link to="/login" type="button" className="btn btn-outline-secondary btn-lg px-4">Login</Link>

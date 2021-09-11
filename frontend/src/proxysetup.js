5ca94665d8a3dfe4e16d3bc6669f0a9021f0586d
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-09-11 16:37:57 +0530

    added the lets-encrypt script

--- frontend/src/proxysetup.js
+++ frontend/src/proxysetup.js
@@ -3,44 +3,44 @@ const proxy = require("http-proxy-middleware")
 module.exports = function(App){
     App.use(
         proxy("add_user",{
-            target:"http://127.0.0.1:8000/",
+            target: "http://backend:8000/",
             changeOrigin : true
         })
     );
    
     App.use(
         proxy("token",{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );
     App.use(
         proxy("get_user/check/{username}",{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );
     App.use(
         proxy("add_url",{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );
     App.use(
         proxy("url-stats",{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );
     App.use(
         proxy(`url-stats/?paging_state=${pagingStatus}`,{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );
     App.use(
         proxy('send-code',{
-            target:"http://127.0.0.1:8000/",
+            target:"http://backend:8000/",
             changeOrigin : true
         })
     );

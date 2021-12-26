c9ce32107d430b3bc68ab04de2f84ef82c86dfbc
CosmicOppai <kanakchaudhari12@gmail.com>
2021-12-26 16:19:25 +0530

    fixed the footer on userPage ✨

--- frontend/src/Mycomponents/pages/components/footer.js
+++ frontend/src/Mycomponents/pages/components/footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
     return (
         <div>
             {user && (
-                <footer className="footer mt-auto py-3 bg-light text-dark">
+                <footer className="footer mt-auto py-3 bg-light text-dark fixed-bottom">
                     <div className="container text-center ">
-                        Copyright &copy;小URL.com
+                        Copyright &copy;小URL
                     </div>
                     <div className="container text-center">
                         <span className="me-2"><a href="https://github.com/Cosmicoppai/chi_url" target={'_blank'} rel="noreferrer"><i className="bi bi-github "></i></a></span>
@@ -22,7 +22,7 @@ const Footer = () => {
             {!user && (
                 <footer className="footer mt-auto py-1 bg-dark text-light">
                     <div className="container text-center ">
-                        Copyright &copy;小URL.com
+                        Copyright &copy;小URL
                     </div>
                     <div className="container text-center">
                         <span className="me-2"><a href="https://github.com/Cosmicoppai/chi_url" target={'_blank'} rel="noreferrer"><i className="bi bi-github "></i></a></span>

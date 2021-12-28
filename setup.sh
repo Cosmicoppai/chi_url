233546af05d3462f9475186782183c05d0a3cd2c
CosmicOppai <kanakchaudhari12@gmail.com>
2021-12-28 17:14:02 +0530

    applied the exec permission to docker-compose binaries ✨

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce
@@ -17,8 +17,8 @@ compose_release() {
 	    }
 
     if ! [ -x "$(command -v docker-compose)" ]; then
-	      curl -L https://github.com/docker/compose/releases/download/$(compose_release)/docker-compose-$(uname -s)-$(uname -m) \
-		        -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+              sudo curl -L https://github.com/docker/compose/releases/download/$(compose_release)/docker-compose-$(uname -s)-$(uname -m) \
+                        -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
     fi
 
 

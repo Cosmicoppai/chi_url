c9d67985ad117285e309e5254d3290030bf0a96e
tnaahsus <72088338+tnaahsus@users.noreply.github.com>
2021-12-31 03:07:55 +0530

    Update verifymail.js

--- frontend/src/Mycomponents/pages/signuppages/verifymail.js
+++ frontend/src/Mycomponents/pages/signuppages/verifymail.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom';
 import Footer from "../components/footer"
 
 const Verifymail = () => {
+    const [loading, setloading] = useState(false)
+    const [sendBox, setsendBox] = useState(true)
+
     const emailVerification = async () => {
+        setloading(true)
         const token = localStorage.getItem("token");
         await axios.get('send-code', {
             headers: {
@@ -14,8 +18,8 @@ const Verifymail = () => {
         })
             .then(resp => {
                 localStorage.clear();
-                document.getElementById('verifyMail').style.display = 'none'
-                document.getElementById('alertBar').style.display = 'flex'
+                setsendBox(false)
+                setloading(false)
             })
             .catch(error => {
                 // console.error(error);
@@ -48,6 +52,7 @@ const Verifymail = () => {
                 </div>
 
             </nav>
+            {sendBox &&(
             <div className=" justify-content-center text-center flex-column" id='verifyMail' style={mystyle}>
                 <h2> Please Verify your email</h2>
                 <p>Please click on the send button to recieve the
@@ -55,11 +60,18 @@ const Verifymail = () => {
                 <button type="button" className="btn btn-dark w-25 mx-auto mb-5"
                     onClick={emailVerification}>Send</button>
             </div>
+            )}
+            {!sendBox &&(
             <div className="alert alert-dark bg-transparent justify-content-center text-center flex-column " id='alertBar' role="alert" style={mystyle2} >
                 <h4>Please click on the link within 15 minutes to verify your account and login again to use our services!!!</h4>
                 <a type="button" style={{ color: 'black' }} href="/login" className="btn btn-lg btn-outline-light ms-2">Go to login page</a>
             </div>
-
+            )}
+            {loading &&(
+                <div className="spinner-border justify-content-center text-center" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            )}
             <Footer />
         </>
     )

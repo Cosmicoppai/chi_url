6f4356ebf6001195ccdf41c1372af3168b58459b
tnaahsus <72088338+tnaahsus@users.noreply.github.com>
2021-12-31 09:49:04 +0530

    Update userpage.js

--- frontend/src/Mycomponents/pages/userpages/userpage.js
+++ frontend/src/Mycomponents/pages/userpages/userpage.js
@@ -12,13 +12,13 @@ const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=
 
 const UserPage = () => {
     const [pagingStatus, setPagingstatus] = useState('');
-    const [datas, setdatas] = useState([]);
-    const [button, setbutton] = useState(false);
-    const [dataValue, setdataValue] = useState(false);
-    const [datas1, setdatas1] = useState([]);
+    const [data, setData] = useState([]);
+    const [moreData, setmoreData] = useState([]);
     const [link, setLink] = useState('');
     const [short, setShort] = useState('');
-    const [url, setUrl] = useState('');
+    const [originDomain, setoriginDomain] = useState('');
+    const [dataTable, setdataTable] = useState(false);
+    const [morebutton, setMorebutton] = useState(false);
     const [error, setError] = useState(false);
     const [redirect, setRedirect] = useState(false);
     const [mobileTable, setmobileTablet] = useState(false);
@@ -57,7 +57,7 @@ const UserPage = () => {
     const getLink = async () => {
         const token = localStorage.getItem("token");
         await axios.post('add_url', {
-            url: link
+            originDomain: link
         }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -70,7 +70,7 @@ const UserPage = () => {
                 // console.log(response.data.short_url); //check the resp
                 setIsLoading(false);
                 if (response.status === 201) {
-                    setUrl(window.location.origin + '/')
+                    setoriginDomain(window.location.origin + '/')
                     setShort(response.data.short_url);
                 }
             })
@@ -86,7 +86,7 @@ const UserPage = () => {
 
     const getUrls = () => {
         const token = localStorage.getItem("token");
-        axios.get('url-stats', {
+        axios.get('originDomain-stats', {
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'accept': 'application/json'
@@ -102,20 +102,20 @@ const UserPage = () => {
                 }
                 let data = resp.data.stats
                 if (data.length === 0) {
-                    setdataValue(false)
+                    setdataTable(false)
                 }
                 else {
-                    setdataValue(true)
+                    setdataTable(true)
                 }
                 if (resp.data.paging_state === null) {
                     setPagingstatus(resp.data.paging_state)
-                    setdatas(resp.data.stats)
-                    setbutton(false)
+                    setData(resp.data.stats)
+                    setMorebutton(false)
                 }
                 else {
                     setPagingstatus(resp.data.paging_state)
-                    setdatas(resp.data.stats)
-                    setbutton(true)
+                    setData(resp.data.stats)
+                    setMorebutton(true)
                 }
 
             })
@@ -135,7 +135,7 @@ const UserPage = () => {
     const moreRequest = async (e) => {
         e.preventDefault();
         const token = localStorage.getItem("token");
-        await axios.get(`url-stats/?paging_state=${pagingStatus}`
+        await axios.get(`originDomain-stats/?paging_state=${pagingStatus}`
             ,
             {
                 headers: {
@@ -147,13 +147,13 @@ const UserPage = () => {
                 // console.log(resp); //check the resp
                 if (resp.data.paging_state === null) {
                     setPagingstatus(resp.data.paging_state);
-                    setdatas1(resp.data.stats);
-                    setbutton(false)
+                    setmoreData(resp.data.stats);
+                    setMorebutton(false)
                 }
                 else {
                     setPagingstatus(resp.data.paging_state);
-                    setdatas1(resp.data.stats);
-                    setbutton(true);
+                    setmoreData(resp.data.stats);
+                    setMorebutton(true);
                 }
             })
             .catch((error) => {
@@ -181,7 +181,7 @@ const UserPage = () => {
                             </div>
                             {error && (
                                 <p className="text-danger " role="alert">
-                                    Yikes! That's not a valid url
+                                    Yikes! That's not a valid originDomain
                                 </p>
                             )}
                             {emptyError && (
@@ -190,24 +190,24 @@ const UserPage = () => {
                                 </p>
                             )}
                             {!isLoading && (
-                                <button onClick={(e) => {
+                                <morebutton onClick={(e) => {
                                     handleSubmit(e)
                                     setTimeout(() => {
                                         getUrls()
                                     }, 5000);
-                                }} type="submit" className="btn btn-primary mb-1 px-5">Submit</button>
+                                }} type="submit" className="btn btn-primary mb-1 px-5">Submit</morebutton>
                             )}
                             {isLoading && (
-                                <button className="btn btn-primary" type="button" disabled>
+                                <morebutton className="btn btn-primary" type="morebutton" disabled>
                                     <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
                                     Loading...
-                                </button>
+                                </morebutton>
                             )}
                         </form>
                         {short && (
                             <div className="d-flex justify-content-center align-items-center text-center flex-column w-100 ">
-                                <h4 ><span type="text" className="badge  bg-dark px-5" >{url}{short}</span> </h4>
-                                <button type="button" id='btnClick' className="btn btn-outline-dark" onClick={() => navigator.clipboard.writeText(`${url}${short}`)}>Copy</button>
+                                <h4 ><span type="text" className="badge  bg-dark px-5" >{originDomain}{short}</span> </h4>
+                                <morebutton type="morebutton" id='btnClick' className="btn btn-outline-dark" onClick={() => navigator.clipboard.writeText(`${originDomain}${short}`)}>Copy</morebutton>
                             </div>
                         )}
                     </div>
@@ -215,7 +215,7 @@ const UserPage = () => {
                     <div className="container mb-5">
                         <div class="center-block fix-width scroll-inner tableMargin">
                             <table class="table1 table-striped text-center table-bordered">
-                                {dataValue && (
+                                {dataTable && (
                                     <>
                                         <thead className="bg-dark text-light" >
                                             <tr>
@@ -225,17 +225,17 @@ const UserPage = () => {
                                             </tr>
                                         </thead>
                                         <tbody className="w-25">
-                                            {(datas || []).map((data, id) => {
+                                            {(data || []).map((data, id) => {
                                                 return <tr key={id}>
-                                                    <td> <a className="visitedLink" href={data.url} rel="noreferrer" target="_blank">{data.url}</a> </td>
+                                                    <td> <a className="visitedLink" href={data.originDomain} rel="noreferrer" target="_blank">{data.originDomain}</a> </td>
                                                     <td><a className="visitedLink" href={data.short_url} rel="noreferrer" target="_blank">{data.short_url}</a></td>
                                                     <td>{data.resolves}</td>
                                                 </tr>
 
                                             })}
-                                            {(datas1 || []).map((data, id) => {
+                                            {(moreData || []).map((data, id) => {
                                                 return <tr key={id}>
-                                                    <td> <a className="visitedLink" href={data.url} rel="noreferrer" target="_blank">{data.url}</a> </td>
+                                                    <td> <a className="visitedLink" href={data.originDomain} rel="noreferrer" target="_blank">{data.originDomain}</a> </td>
                                                     <td><a className="visitedLink" href={data.short_url} rel="noreferrer" target="_blank">{data.short_url}</a></td>
                                                     <td>{data.resolves}</td>
                                                 </tr>
@@ -253,7 +253,7 @@ const UserPage = () => {
                     {!mobileTable &&(
                     <div className="container mb-5">
                         <table className="table table-bordered text-center tableMargin">
-                            {dataValue && (
+                            {dataTable && (
                                 <>
 
                                     <thead className="table-dark" >
@@ -264,17 +264,17 @@ const UserPage = () => {
                                         </tr>
                                     </thead>
                                     <tbody className="w-25">
-                                        {(datas || []).map((data, id) => {
+                                        {(data || []).map((data, id) => {
                                             return <tr key={id}>
-                                                <td> <a className="visitedLink" href={data.url} rel="noreferrer" target="_blank">{data.url}</a> </td>
+                                                <td> <a className="visitedLink" href={data.originDomain} rel="noreferrer" target="_blank">{data.originDomain}</a> </td>
                                                 <td><a className="visitedLink" href={data.short_url} rel="noreferrer" target="_blank">{data.short_url}</a></td>
                                                 <td>{data.resolves}</td>
                                             </tr>
 
                                         })}
-                                        {(datas1 || []).map((data, id) => {
+                                        {(moreData || []).map((data, id) => {
                                             return <tr key={id}>
-                                                <td> <a className="visitedLink" href={data.url} rel="noreferrer" target="_blank">{data.url}</a> </td>
+                                                <td> <a className="visitedLink" href={data.originDomain} rel="noreferrer" target="_blank">{data.originDomain}</a> </td>
                                                 <td><a className="visitedLink" href={data.short_url} rel="noreferrer" target="_blank">{data.short_url}</a></td>
                                                 <td>{data.resolves}</td>
                                             </tr>
@@ -286,8 +286,8 @@ const UserPage = () => {
 
                             )}
                         </table>
-                        {button && (
-                            <button type="button" className="btn btn-dark mt-2 d-grid mx-auto btn-lg mb-3" onClick={moreRequest}>More</button>
+                        {morebutton && (
+                            <morebutton type="morebutton" className="btn btn-dark mt-2 d-grid mx-auto btn-lg mb-3" onClick={moreRequest}>More</morebutton>
                         )}
                     </div>
                     )}
